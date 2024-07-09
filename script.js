bca3a6f0c3b5d840f1733159251a53a4d0957cc5
soliha02 <164037162+soliha02@users.noreply.github.com>
2024-07-09 22:54:08 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let body = document.querySelector('body')
+let color = prompt('RANG KIRITING!')
+let colora = prompt('TEXT RANGINI KIRITING!')
+
+body.style.backgroundColor = color
+colora.style.color = color
+body.style.textAlign = 'center'
+body.style.marginTop = '250px'
+
